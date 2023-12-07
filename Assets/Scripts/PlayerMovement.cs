513facd3197221e0956bee3a15c6ea7efa5be5c2
0led <oldrichubinak@gmail.com>
2023-12-07 17:10:01 +0100

    init, fix, movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 2;
+    public float jumpForce = 5;
+    public Rigidbody2D _rigidbody;
+    private float lastJumpTime = 0.0f;
+    private float jumpCooldown = 0.5f;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+     void Update()
+    {
+    
+        if (gameObject.tag == "Player1")
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector2.left * speed * Time.smoothDeltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector2.right * speed * Time.smoothDeltaTime);
+            }
+            if (Input.GetKeyDown(KeyCode.W) && Time.time - lastJumpTime > jumpCooldown)
+            {
+                _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                lastJumpTime = Time.time;
+            }
+        }
+
+        if (gameObject.tag == "Player2")
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(Vector2.left * speed * Time.smoothDeltaTime);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(Vector2.right * speed * Time.smoothDeltaTime);
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow) && Time.time - lastJumpTime > jumpCooldown)
+            {
+                _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                lastJumpTime = Time.time;
+            }
+        }
+    }
+}
