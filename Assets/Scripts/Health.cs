b832f708b45c7be2080ed23b5ed4747e492e1692
0led <oldrichubinak@gmail.com>
2024-02-22 14:55:24 +0100

    health_number_add

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,8 +7,11 @@ public class Health : MonoBehaviour
 {
     public Image healthBar1;
     public Image healthBar2;
+    public Text healthText1;
+    public Text healthText2;
     private Image selectedHealthBar;
-    float health, maxHealth = 100;
+    private Text selectedHealthText;
+    float health, maxHealth = 200;
     float lerpSpeed;
 
     void Start()
@@ -18,10 +21,12 @@ public class Health : MonoBehaviour
         if (gameObject.tag == "Player1")
         {
             selectedHealthBar = healthBar1;
+            selectedHealthText = healthText1;
         }
         else if (gameObject.tag == "Player2")
         {
             selectedHealthBar = healthBar2;
+            selectedHealthText = healthText2;
         }
     
     }
@@ -30,8 +35,15 @@ public class Health : MonoBehaviour
     {
         if (health > maxHealth) health = maxHealth;
         lerpSpeed = 3f * Time.deltaTime;
+        
         HealthBarFiller();
         ColorChanger();
+
+        // Aktualizace zdravotního textu
+        if (selectedHealthText != null)
+        {
+            selectedHealthText.text = health.ToString("0"); // Zobrazí zdraví jako celé číslo
+        }
     }
 
     public void TakeDamage(float damage)
